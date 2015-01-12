28449e8147451c552ef0279143ec574b0df3df1c
Fabio C. Barrionuevo da Luz <bnafta@gmail.com>
2015-01-11 21:24:46 -0300

    added compatibility layer using python-future

--- tests/cindex/test_cdb.py
+++ tests/cindex/test_cdb.py
@@ -1,3 +1,10 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+from builtins import *
 from clang.cindex import CompilationDatabase
 from clang.cindex import CompilationDatabaseError
 from clang.cindex import CompileCommands
--- tests/cindex/test_code_completion.py
+++ tests/cindex/test_code_completion.py
@@ -1,3 +1,10 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+from builtins import *
 from clang.cindex import TranslationUnit
 
 def check_completion_results(cr, expected):
